f4c89794f51d15a8c7ac1ea9ec67029cb3641a61
quochoangdev <quochoangdev.official@gmail.com>
2025-05-20 18:56:35 +0700

    Add WooCommerce templates for cart and checkout functionality; include cart page, mini-cart, and various cart-related templates for improved shopping experience. Update app.css to comment out touch-action style for better touch interaction consistency.

---
+++ wp-content/themes/ryvances/templates/checkout.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template Name: Checkout
+ *
+ * @author ryan
+ */
+
+get_header();
+?>
+<?php get_template_part('template-customs/rank_math_the_breadcrumbs'); ?>
+
+<main>
+  <section class="bg-bg-primary">
+    <div class="container px-4 mx-auto h-full">
+      <?php the_content(); ?>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
